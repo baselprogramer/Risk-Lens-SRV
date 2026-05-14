c6c636c3c892f6eaea5d638d8962ad4e207ad498
baselprogramer <prencbasel2000@gmail.com>
2026-05-14 11:28:39 +0300

    blacklist try

--- src/main/java/com/sdn/blacklist/BlacklistApplication.java
+++ src/main/java/com/sdn/blacklist/BlacklistApplication.java
@@ -19,16 +19,12 @@ package com.sdn.blacklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // Add this
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-// 1. Tell Spring exactly where your Postgres repositories are
-@EnableJpaRepositories(basePackages = "com.sdn.blacklist.repository") 
-// 2. Tell Spring exactly where your Elasticsearch repositories are (if you have them)
-@EnableElasticsearchRepositories(basePackages = "com.sdn.blacklist.search.repository") 
+@EnableJpaRepositories(basePackages = "com.sdn.blacklist") // This tells Spring to find TenantRepository
 public class BlacklistApplication {
 
     public static void main(String[] args) {
