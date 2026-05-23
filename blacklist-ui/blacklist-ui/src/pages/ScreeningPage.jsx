a1bb6c6b635d3542387315c8d46e7901f6626b8f
baselprogramer <prencbasel2000@gmail.com>
2026-05-23 14:43:36 +0300

    try

--- blacklist-ui/blacklist-ui/src/pages/ScreeningPage.jsx
+++ blacklist-ui/blacklist-ui/src/pages/ScreeningPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import Layout from "../components/Layout";
 import { createScreeningRequest } from "../services/screeningService";
 import { getPersonDetails } from "../services/searchService";
